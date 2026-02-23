1d5e86fde7e1fb4b85301d70219036a3fd72c08b
abhi <test@test.com>
2026-02-23 21:18:39 +0530

    Initial commit

---
+++ HttpTrigger1.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace HelloFunction;
+
+public class HttpTrigger1
+{
+    private readonly ILogger<HttpTrigger1> _logger;
+
+    public HttpTrigger1(ILogger<HttpTrigger1> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HttpTrigger1")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+    {
+        // Read Environment Variable
+        string message = Environment.GetEnvironmentVariable("MyMessage");
+
+        var response = req.CreateResponse();
+        await response.WriteStringAsync($"Message from Azure: {message}");
+        return response;
+    }
+}
